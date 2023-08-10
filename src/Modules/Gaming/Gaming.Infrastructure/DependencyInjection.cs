b0ceaa4a4a512e87a03da4b31761b8e7414327ad
Daniil Dukhovnyi <dduhovny26@gmail.com>
2023-08-10 23:13:15 +0300

    Конфигурация инфраструктуры

---
+++ src/Modules/Gaming/Gaming.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Gaming.Infrastructure.Messaging;
+using Gaming.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gaming.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureLayerForGaming(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddEntityFrameworkCore(configuration);
+        services.AddRabbit(configuration);
+        
+        return services;
+    }
+}
